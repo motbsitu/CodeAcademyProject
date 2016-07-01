041f1131c1fd4f7b6b1fee7416fab1069a4cd062
Julie S. Mike <juliesmike@gmail.com>
2016-07-01 13:29:27 -0500

    more misc bs

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ console.log("You are at a Justin Bieber concert, and you hear this lyric 'Lace m
 console.log("Suddenly, Bieber stops and says, 'Who wants to race me?'");
 
 var userAnswer = prompt ("Do you want to race Bieber on stage?");
-if (userAnswer = "yes")
+if (userAnswer === "yes")
 {
     console.log("You and Bieber start racing. It's neck and neck! You win by a shoelace!");
 }
@@ -30,3 +30,22 @@ else
 {
     console.log("I'll keep practicing coding and racing.");
 }
+
+var greeting = function(name) {
+    console.log("Hello" + " " + name);
+}
+greeting ("Julie");
+
+var foodDemand = function (food) {
+    console.log ("I want to eat" + " " + food);
+}
+foodDemand("pizza");
+
+
+var orangeCost = function (number) {
+    var val = number * 5;
+    console.log(val);
+};
+orangeCost (10);
+
+    
